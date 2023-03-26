1526ea8077ab3aa4b15c4464e432df3a2f03295c
OrderedSet <orderedset86@gmail.com>
2023-03-26 13:27:25 -0700

    Update backend port

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-    schema: 'http://127.0.0.1:8080/graphql',
+    schema: 'http://127.0.0.1:5000/graphql',
     documents: ['src/**/*.tsx'],
     generates: {
         './src/__generated__/': {
